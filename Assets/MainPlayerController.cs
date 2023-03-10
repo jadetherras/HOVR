4127d13ba2583d98f2f5f50c7380312e1719dd6e
jadetherras <jade.therras@epfl.ch>
2023-03-10 21:59:47 +0100

    Add a simple primitive to detect user's action

---
+++ Assets/MainPlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MainPlayerController : MonoBehaviour {
+
+	/* ... */
+
+	void Start () { /* ... */ }
+
+	void Update () {
+		if( OVRInput.Get( OVRInput.Button.One ) ) Debug.LogWarning( "Button is pressed" );
+	}
+
+	/* ... */
+}
