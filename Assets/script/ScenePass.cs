7dd7341a156861b23b0ebe8f33cc3204c50e0056
jadetherras <jade.therras@epfl.ch>
2023-04-18 10:40:27 +0200

    implement scene changer and basic setting for level 1

---
+++ Assets/script/ScenePass.cs
@@ -0,0 +1,38 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenePass : InteractiveItem
+
+{
+    public enum Scenepass : int { Restart, LevelUp, LevelDown, Default};
+
+    [Header( "Scenepass" )]
+    public Scenepass scenepass;
+    public int scenenb=0;
+
+    void Start(){
+        switch(scenepass) 
+        {
+        case Scenepass.Restart:
+            scenenb = SceneManager.GetActiveScene().buildIndex;
+            break;
+        case Scenepass.LevelUp:
+            scenenb = SceneManager.GetActiveScene().buildIndex+1;
+            break;
+        case Scenepass.LevelDown:
+            scenenb = SceneManager.GetActiveScene().buildIndex-1;
+            break;
+        default:
+            break;
+        }
+	}
+
+    public override void interacted_with ( MainPlayerController player ) { Pass(); }
+
+    public override void exit ( MainPlayerController player ) {}
+
+    public void Pass() {
+        SceneManager.LoadScene(scenenb);
+    }
+
+}
