c3b99b3864eb8318ff97bd2ddc676edc0bdd9373
jadetherras <jade.therras@epfl.ch>
2023-04-23 01:08:33 +0200

    screen

--- Assets/script/screen.cs
+++ Assets/script/screen.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class screen : InteractiveItem {
 	
 	public GameObject plate;
+	public VideoPlayer V;
+	public GameObject video;
 	public bool Isactive = false;
 	//public int i = 0;
 	void Start () {
 			//plate = GameObject.FindGameObjectWithTag("plate");
 			plate.SetActive(Isactive);
+			video.SetActive(Isactive);
+			V = video.GetComponent<VideoPlayer>();
 	}
 
 	//void Update() {
@@ -22,10 +27,14 @@ public class screen : InteractiveItem {
 
 	public override void interacted_with( MainPlayerController player ) {
 		plate.SetActive(true);
+		video.SetActive(true);
+		V.GetComponent<VideoPlayer>().Play();
 	}
 
 	public override void exit( MainPlayerController player ) {
 		plate.SetActive(false);
+		video.SetActive(false);
+		V.GetComponent<VideoPlayer>().Stop();
 	}
 
 }
