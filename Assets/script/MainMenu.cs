5d29a7e84d0da073353abf2d16dcb2cb6b76fd1b
Lena de Sepibus <lena.desepibus@epfl.ch>
2023-05-23 16:00:18 +0200

    end_menu

---
+++ Assets/script/MainMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayTuto() {  
+        SceneManager.LoadScene("tutorial");  
+    }
+     
+    public void PlayLevel1() {  
+        SceneManager.LoadScene("level1");  
+    }
+
+    public void PlayLevel2() {  
+        SceneManager.LoadScene("level2");  
+    }  
+
+    public void PlayLevel3() {  
+        SceneManager.LoadScene("level3");  
+    }  
+
+    public void PlayQuit() {  
+        Application.Quit();  
+    } 
+     
+
+    //pas sûre que ça fonctionne !!
+    public void QuitGame() {  
+        Debug.Log("QUIT");  
+        SceneManager.LoadScene("Quit");  
+    }  
+      
+
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
